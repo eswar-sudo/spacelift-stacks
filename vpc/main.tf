5e70c44cc0545e173a7db2e58e7cb33b560d72f0
eswar-sudo <eswarkumar501@gmail.com>
2025-08-20 21:07:50 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -3,14 +3,14 @@ module "vpc-modules" {
   version = "0.1.0"
 
   # Required inputs
-  azs                            = # list(string)
-  enable_dns_hostnames           = # bool
-  enable_dns_support             = # bool
-  map_public_ip_on_launch        = # bool
-  private_destination_cidr_block = # string
-  public_destination_cidr_block  = # string
-  region                         = # string
-  single_nat_gateway             = # bool
-  vpc_cidr                       = # string
-  vpc_name                       = # string
+  azs                            = ["us-east-1a", "us-east-1b"]
+  enable_dns_hostnames           = true
+  enable_dns_support             = true
+  map_public_ip_on_launch        = true
+  private_destination_cidr_block = "0.0.0.0/0"
+  public_destination_cidr_block  = "0.0.0.0/0"
+  region                         = "us-east-1"
+  single_nat_gateway             = true
+  vpc_cidr                       = "30.0.0.0/16"
+  vpc_name                       = "stack-testing"
 }
