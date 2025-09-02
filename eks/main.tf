af84086e75fa07b47bda40f1fd2c503e06dab1fa
eswar-sudo <eswarkumar501@gmail.com>
2025-09-02 10:49:24 +0530

    Create main.tf

---
+++ eks/main.tf
@@ -0,0 +1,29 @@
+module "eks" {
+  source  = "spacelift.io/eswar-sudo/eks/default"
+  version = "0.1.0"
+
+  # Required inputs
+  ami_type           = "AL2023_x86_64_STANDARD"
+  capacity_type      = "ON_DEMAND"
+  cluster_name       = "eks-dns"
+  cluster_version    = "1.33"
+  coredns_version    = "v1.11.4-eksbuild.2"
+  desired_size       = 2
+  disk_size          = 20
+  instance_types     = ["t3.xlarge"]
+  kube_proxy_version = "v1.32.0-eksbuild.2"
+  labels             = {
+    nodegroup = "system"
+    }
+  max_size           = 3
+  min_size           = 1
+  node_group_name    = "linux-ng"
+  private_subnet_ids = ["subnet-0134c2b93c7390d1b", "subnet-0c2538a03427b4d8f"]
+  region             = "us-east-1"
+  tags               = {
+    Environment = "dev"
+    Terraform   = "true"
+    }
+  vpc_cni_version    = "v1.19.6-eksbuild.1"
+  vpc_id             = "vpc-03ff1f601bf029bbd"
+}
