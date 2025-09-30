ebb94807bee568374a820cd5ae7b52362322c1d8
eswar-sudo <eswarkumar501@gmail.com>
2025-09-30 12:47:15 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -3,13 +3,13 @@ module "vpc-modules" {
   version = "0.1.0"
 
   # Required inputs
-  azs                            = ["us-east-1a", "us-east-1b"]
+  azs                            = ["ap-south-1a", "ap-south-1b"]
   enable_dns_hostnames           = true
   enable_dns_support             = true
   map_public_ip_on_launch        = true
   private_destination_cidr_block = "0.0.0.0/0"
   public_destination_cidr_block  = "0.0.0.0/0"
-  region                         = "us-east-1"
+  region                         = "ap-south-1"
   single_nat_gateway             = true
   vpc_cidr                       = "30.0.0.0/16"
   vpc_name                       = "eks-alb"
